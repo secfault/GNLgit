efb6f32f29e2022d07184d27a3e6901e30ea39e8
Dumont TONY <dtony@e2r9p16.42.fr>
2018-12-19 20:03:52 +0100

    jai plus de cerveau

--- get_next_line.c
+++ get_next_line.c
@@ -6,21 +6,29 @@
 /*   By: dtony <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/17 17:22:02 by dtony             #+#    #+#             */
-/*   Updated: 2018/12/17 19:59:56 by dtony            ###   ########.fr       */
+/*   Updated: 2018/12/19 20:03:22 by dtony            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "./libft/libft.h"
 #include "get_next_line.h"
+
+char	*linefiller(char *dest, char *buf, char c)
+{
+	if (dest && buf && c)
+		dest = ft_strrsub(buf, ft_strchr(buf, c),
+						  ft_strrlen(ft_strchr(buf, c)));
+}
 
 int		get_next_line(const int fd, char **line)
 {
-	size_t	nbr;
-	reader	linus;
+	t_line	linus;
+	char	*temp;
+	char	buf[BUFF_SIZE + 1];
 
-	linus.fd = fd;
-	linus.ret = read(linus.fd, linus.buff, BUFF_SIZE);
-	while (linus.ret)
-		ft_strendl(linus.buff);
-	return (0);
+	if (line && fd > 0)
+	{
+		while ((linus.ret = read(fd, buf, BUFF_SIZE)))
+		{
+			linefiller(temp, buf, '\n');
+			ft_strjoin(*line, 
 }
