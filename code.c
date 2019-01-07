a2afe298aa00fa89dd264fa4bd025c73c9b7058f
Dumont TONY <dtony@e2r9p20.42.fr>
2019-01-07 19:34:28 +0100

    fin de journee gnl modifier a tester

---
+++ code.c
@@ -0,0 +1,1 @@
+yyytyt78t8uugughgugu g89y 8y8i hhihhii\];p-0n
--- gnl.c
+++ gnl.c
@@ -6,21 +6,24 @@
 /*   By: dtony <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/07 15:49:05 by dtony             #+#    #+#             */
-/*   Updated: 2019/01/07 16:40:00 by dtony            ###   ########.fr       */
+/*   Updated: 2019/01/07 19:32:46 by dtony            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*linefiller(char *dest, char *buf, char c)
+static char	*linefiller(char *buf, char c)
 {
-	if (dest && buf && c)
+	char	*dest;
+
+	dest = NULL;
+	if (buf && c)
 		dest = ft_strrsub(buf, ft_strchr(buf, c),
 						  ft_strrlen(ft_strchr(buf, c)));
 	return (dest);
 }
 
-int		get_next_line(const int fd, char **line)
+int			get_next_line(const int fd, char **line)
 {
 	int			ret;
 	static char	*stock;
@@ -28,13 +31,29 @@ int		get_next_line(const int fd, char **line)
 
 	if (fd < 0 || !(line))
 		return (-1);
+	if (stock && ft_strchr(stock, '\n'))
+	{
+		*line = linefiller(stock, '\n');
+		stock = ft_strdup(ft_strchr(stock, '\n'));
+		ft_putstr(*line);
+		return (1);
+	}
 	while ((ret = read(fd, buf, BUFF_SIZE)))
 	{
+		buf[BUFF_SIZE + 1] = '\0';
 		if (ft_strchr(buf, '\n'))
-			*line = linefiller(*line, buf, '\n');
+		{
+			*line = ft_strmcat(*line, linefiller(*line, buf, '\n'));
+			ft_putstr(*line);
+			free(stock);
+			stock = ft_strdup(ft_strchr(buf, '\n');
+			break;
+		}
+		else
+		{
+			*line = ft_strmcat(*line, buf);
+			stock = (stock) ? ft_strmcat(stock, buf) : ft_strdup(buf);
+		}
 	}
-	if (ft_strchr(stock, '\n'))
-	{
-		*line = ft_strmcat(*line, stock, '\n');
-		stock = ft_strchr(stock, '\n');
-	}
+	
+}
