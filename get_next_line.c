642c91a74fc4b6e97add113f5efa949d821c30dd
secfault <dumont_t@etna-alternance.net>
2019-02-21 11:24:37 +0100

    end need norme

--- get_next_line.c
+++ get_next_line.c
@@ -14,14 +14,14 @@
 
 static int			checkstock(int fd, char **stock, char **line)
 {
-	char	*start;
+	char	*end;
 	char	*tmp;
 
 	tmp = NULL;
 	if (stock[fd] && ft_strchr(stock[fd], '\n'))
 	{
-		start = ft_strchr(stock[fd], '\n');
-		*line = ft_strsub(stock[fd], 0, start - stock[fd]);
+		end = ft_strchr(stock[fd], '\n');
+		*line = ft_strsub(stock[fd], 0, end - stock[fd]);
 		tmp = ft_strdup(stock[fd]);
 		free(stock[fd]);
 		stock[fd] = ft_strdup(ft_strchr(tmp, '\n') + 1);
@@ -39,18 +39,32 @@ static int			checkstock(int fd, char **stock, char **line)
 
 static int			readline(char **stock, char **line, char *buf, int fd)
 {
-	char	*start;
+	char	*end;
 
 	if (buf[0] == '\n')
-		stock[fd] = ft_strdup(ft_strchr(buf, '\n') + 1);
+	{
+		*line = ft_strdup(stock[fd]);
+		free(stock[fd]);
+		if ((ft_strchr(buf, '\n') + 1) != '\0')
+			stock[fd] = ft_strdup(ft_strchr(buf, '\n') + 1);
+		// ft_putendl("fuq");
+	}
 	else
 	{
-		start = ft_strchr(buf, '\n');
-		*line = ft_strsub(buf, 0, start - buf);
-		stock[fd] = ft_strdup(ft_strchr(buf, '\n') + 1);
-		return (1);
+		end = ft_strchr(buf, '\n');
+		*line = ft_strjoin(stock[fd], ft_strsub(buf, 0, end - buf));
+		if (stock[fd])
+			free(stock[fd]);
+		if ((ft_strchr(buf, '\n') + 1) != '\0')
+			stock[fd] = ft_strdup(ft_strchr(buf, '\n') + 1);
+		// ft_putendl("yes");
 	}
-	return (0);
+	// ft_putstr("\nligne :");
+	// ft_putendl(*line);
+	// ft_putstr("\nstock :");
+	// ft_putendl(stock[fd]);
+	// ft_putendl("\n*ligne lue*");
+	return (1);
 	
 }
 
@@ -62,17 +76,23 @@ int					get_next_line(const int fd, char **line)
 
 	if (fd < 0 || !(line))
 		return (-1);
-	if (checkstock(fd, stock, line))
-		return (1);
 	ft_bzero(buf, ft_strlen(buf));
 	while ((ret = read(fd, buf, BUFF_SIZE)))
 	{
 		buf[ret] = '\0';
+		// ft_putendl("");
+		// ft_putstr("buf = ");
+		// ft_putstr(buf);
+		// ft_putendl("\nFIN");
 		if (ft_strchr(buf, '\n') &&
 				readline(stock, line, buf, fd))
-			return (1);
+					return (1);
 		else
 			stock[fd] = ft_strjoin(stock[fd], buf);
+		// ft_putendl("");
+		// ft_putstr("stock = ");
+		// ft_putstr(stock[fd]);
+		// ft_putendl("\nFIN");
 		ft_bzero(buf, ft_strlen(buf));
 	}
 	if (checkstock(fd, stock, line))
