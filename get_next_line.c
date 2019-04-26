886ea98655e5862465552467c242c404717dea98
Dumont TONY <dtony@e2r4p20.42.fr>
2019-04-26 11:15:07 +0200

    gnl heal

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: dtony <dtony@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/31 20:16:11 by dtony             #+#    #+#             */
-/*   Updated: 2019/01/31 20:16:11 by dtony            ###   ########.fr       */
+/*   Updated: 2019/04/26 11:10:42 by dtony            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,6 @@ static int			checkstock(int fd, char **stock, char **line)
 	{
 		end = ft_strchr(stock[fd], '\n');
 		*line = ft_strsub(stock[fd], 0, end - stock[fd]);
-		free(end);
 		tmp = ft_strdup(stock[fd]);
 		free(stock[fd]);
 		stock[fd] = ft_strdup(ft_strchr(tmp, '\n') + 1);
@@ -32,7 +31,7 @@ static int			checkstock(int fd, char **stock, char **line)
 	else if (stock[fd] && stock[fd][0])
 	{
 		*line = ft_strdup(stock[fd]);
-		free(stock[fd]);
+		//free(stock[fd]);
 		return (1);
 	}
 	return (0);
@@ -45,7 +44,8 @@ static int			readline(char **stock, char **line, char *buf, int fd)
 	if (buf[0] == '\n')
 	{
 		*line = ft_strdup(stock[fd]);
-		free(stock[fd]);
+		if(stock[fd])
+			free(stock[fd]);
 		if ((ft_strchr(buf, '\n') + 1) != '\0')
 			stock[fd] = ft_strdup(ft_strchr(buf, '\n') + 1);
 	}
@@ -53,8 +53,6 @@ static int			readline(char **stock, char **line, char *buf, int fd)
 	{
 		end = ft_strchr(buf, '\n');
 		*line = ft_strjoin(stock[fd], ft_strsub(buf, 0, end - buf));
-		end = NULL;
-		free(end);
 		if (stock[fd])
 			free(stock[fd]);
 		if ((ft_strchr(buf, '\n') + 1) != '\0')
@@ -69,10 +67,10 @@ int					get_next_line(const int fd, char **line)
 	static char	*stock[999];
 	char		buf[BUFF_SIZE + 1];
 
-	if (fd < 0 || !(line))
+	if (fd < 0 || !(line) || read(fd, buf, 0) < 0)
 		return (-1);
 	ft_bzero(buf, ft_strlen(buf));
-	while ((ret = read(fd, buf, BUFF_SIZE)))
+	while ((ret = read(fd, buf, BUFF_SIZE)) > 0)
 	{
 		buf[ret] = '\0';
 		if (ft_strchr(buf, '\n') &&
